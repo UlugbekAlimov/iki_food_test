ea808c4bbdfa47bb7c757688e5be0887467b3030
Улугбек Алимов <alimovu966@gmail.com>
2024-04-30 07:47:25 +0500

    commit

--- src/components/hexBlock.vue
+++ src/components/hexBlock.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <div @mouseenter="isHovered = true" @mouseleave="isHovered = false" class="w-auto h-[174px] laptop:w-[278px] laptop:h-[174px] desktop:w-[447px] desktop:h-[280px]
-      rounded-[15px] relative flex flex-col desktop:p-10 laptop:pl-[30px] laptop:pt-[40px] pl-[30px] pt-[40px] main" :style="{ backgroundColor: color.hex }">
+    <div @mouseenter="isHovered = true" @mouseleave="isHovered = false" class="w-auto h-[174px] laptop:w-full laptop:h-[174px] desktop:w-[447px] desktop:h-[280px]
+      rounded-[15px] flex flex-col desktop:p-10 laptop:pl-[30px] laptop:pt-[40px] px-[30px] pt-[40px] main" :style="{ backgroundColor: color.hex }">
       <div class="img">
         <div :style="{ backgroundColor: color.hex }" class=""></div>
         <p :style="{ color: fontColor }" class="text-[#ffff] desktop:text-[32px] laptop:text-[20px] text-[20px] desktop:my-[15px]">{{ title }}</p>
@@ -11,8 +11,8 @@
           <div>CMYK: {{ color.cmyk }}</div>
       </div>
       </div>
-      <div :class="isHovered ? 'opacity-100' : 'opacity-0' " class="absolute desktop:bottom-6 laptop:bottom-6 bottom-6 transition-all duration-300">
-        <button class="copy-button desktop:w-[360px] desktop:text-[28px] desktop:h-[51px] laptop:w-[215px] w-[285px] h-[30px] laptop:text-[17px] text-[#00A6ED] laptop:h-[30px] rounded-[7px] border-2 border-[#00A6ED] bg-white hover:bg-[#F6F7F8] ransition-colors duration-300" @click="copyHexCode">{{ buttonText }}</button>
+      <div :class="isHovered ? 'opacity-100' : 'opacity-0' " class="flex justify-center desktop:bottom-6 laptop:bottom-6 bottom-6 transition-all duration-300">
+        <button class="copy-button desktop:w-[360px] desktop:text-[28px] desktop:h-[51px] laptop:w-[215px] w-full h-[30px] laptop:text-[17px] text-[#00A6ED] laptop:h-[30px] rounded-[7px] border-2 border-[#00A6ED] bg-white hover:bg-[#F6F7F8] ransition-colors duration-300" @click="copyHexCode">{{ buttonText }}</button>
       </div>
 
     </div>
